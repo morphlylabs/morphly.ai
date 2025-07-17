9598f7ae80df17d8f9b6242e9839b239f6147557
Ruslan Zabarov <ruszabarov@gmail.com>
2025-07-17 13:27:31 -0400

    feat: add sign out

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -1,26 +1,46 @@
-import Link from "next/link";
 import { authClient } from "~/lib/auth-client";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import React from "react";
+import {
+  DropdownMenuContent,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+} from "~/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 
 export function UserAvatar() {
   const { data: session } = authClient.useSession();
+  const router = useRouter();
+
+  const signOut = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/sign-in");
+        },
+      },
+    });
+  };
 
   return (
-    <Link
-      href={session?.session ? "/account" : "/sign-in"}
-      className="flex justify-center"
-    >
-      <Avatar>
-        <AvatarImage
-          src={session?.user.image ?? "https://github.com/shadcn.png"}
-          alt={session?.user.name ?? "User avatar"}
-        />
-        <AvatarFallback>
-          {session?.user.name?.charAt(0).toUpperCase() ??
-            session?.user.email?.charAt(0).toUpperCase() ??
-            "U"}
-        </AvatarFallback>
-      </Avatar>
-    </Link>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="hover:cursor-pointer hover:opacity-80">
+          <AvatarImage
+            src={session?.user.image ?? "https://github.com/shadcn.png"}
+            alt={session?.user.name ?? "User avatar"}
+          />
+          <AvatarFallback>
+            {session?.user.name?.charAt(0).toUpperCase() ??
+              session?.user.email?.charAt(0).toUpperCase() ??
+              "U"}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={signOut}>Log out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
