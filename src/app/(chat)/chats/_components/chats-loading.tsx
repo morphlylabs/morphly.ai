460deb51a9f414ba7409f8c5301d82574119df84
Ruslan Zabarov <ruszabarov@gmail.com>
2025-08-20 00:44:30 -0400

    feat: add skeleton for home page

--- src/app/(chat)/chats/_components/chats-loading.tsx
+++ src/app/(chat)/chats/_components/chats-loading.tsx
@@ -1,26 +1,12 @@
 import { Skeleton } from '~/components/ui/skeleton';
+import { ChatPreviewLoader } from '../../../_components/chat-preview-loader';
 
 export function ChatsLoading({ count = 9 }: { count?: number }) {
   return (
     <div className="flex flex-col gap-4">
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         {Array.from({ length: count }).map((_, i) => (
-          <div
-            key={i}
-            className="bg-card text-card-foreground h-full cursor-pointer rounded-xl border shadow-sm transition-shadow hover:shadow-md"
-          >
-            {/* CardHeader skeleton */}
-            <div className="mt-1 flex flex-col space-y-2 p-6">
-              <Skeleton className="h-5 w-3/4" />
-              <Skeleton className="h-4 w-1/2" />
-            </div>
-            {/* CardContent skeleton */}
-            <div className="p-6 pt-0">
-              <div className="bg-muted flex aspect-video w-full items-center justify-center rounded-lg">
-                <Skeleton className="h-4 w-24" />
-              </div>
-            </div>
-          </div>
+          <ChatPreviewLoader key={i} />
         ))}
       </div>
       {/* Pagination skeleton */}
