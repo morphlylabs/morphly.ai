b70faaca5adbf656ed8985a7910f2dce840c2aff
Ruslan Zabarov <ruszabarov@gmail.com>
2025-08-31 23:10:42 -0400

    feat: create landing page project

---
+++ projects/www/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Toaster } from '@workspace/ui/components/sonner';
+
+export const metadata = {
+  title: 'morphly',
+  description: 'The best text-to-CAD platform',
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+        <Toaster position="bottom-left" />
+      </body>
+    </html>
+  );
+}
