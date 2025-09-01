3814754d50ff0339f6ecec7e40121fb3f359547a
Ruslan Zabarov <ruszabarov@gmail.com>
2025-08-31 23:10:42 -0400

    feat: create landing page project

---
+++ projects/www/eslint.config.js
@@ -0,0 +1,14 @@
+import baseConfig, { restrictEnvAccess } from '@workspace/eslint-config/base';
+import nextjsConfig from '@workspace/eslint-config/next';
+import reactConfig from '@workspace/eslint-config/react';
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+  {
+    ignores: ['.next/**'],
+  },
+  ...baseConfig,
+  ...reactConfig,
+  ...nextjsConfig,
+  ...restrictEnvAccess,
+];
